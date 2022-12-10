137551c704abc80d94d81037144a35472f8bc507
Ammar1201 <aamour1201@gmail.com>
2022-12-11 00:20:58 +0200

    redirect to /api/users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', indexRouter);
+app.get('/', (req, res) => res.redirect('/api/users'));
 
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
